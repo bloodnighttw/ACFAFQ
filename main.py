1f0149eda1e3285876db7333bc4a9787429edb09
bloodnighttw <bbeenn1227@gmail.com>
2023-03-05 17:42:38 +0800

    1. Refactor code. 2. Rewrite the part of getting the refer link.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import random
 import string
 import time
 
-import win32clipboard
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
@@ -10,33 +9,45 @@ from urlextract import URLExtract
 from xtempmail import Email
 from xtempmail.mail import EmailMessage, EMAIL
 from selenium import webdriver
-from selenium.webdriver.edge.options import Options
 from webdriver_auto_update import check_driver
+from fake_useragent import UserAgent
 
-path = "wd/chromedriver.exe"
-
-check_driver('wd')
+####################################################################################
 
 # Remove this and Change Username you want like this : firstEmail = 'type your username here'
 account = ''.join(random.choice(string.ascii_letters) for i in range(10))
 # Change your password here
 password = "0p;/9ol."
 
-option = Options()
+####################################################################################
+
+
+path = "wd/chromedriver.exe"
+check_driver('wd')
+
+option = webdriver.ChromeOptions()
 option.add_argument("--headless")
+option.add_argument("−−lang=en")
+ua = UserAgent()
+userAgent = ua.random
+option.add_argument(f'--user-agent={userAgent}')
+print(userAgent)
 s = Service(path)
 
-secondEmail = ''.join(random.choice(string.ascii_letters) for i in range(10))
+second_account = ''.join(random.choice(string.ascii_letters) for i in range(10))
 
 extractor = URLExtract()
-driver1 = webdriver.Chrome(service=s)
+driver1 = webdriver.Chrome(service=s, options=option)
 
-app1 = Email(name=account, ext=EMAIL.MAILTO_PLUS)
-app2 = Email(name=secondEmail, ext=EMAIL.MAILTO_PLUS)
+email1 = Email(name=account, ext=EMAIL.MAILTO_PLUS)
+email2 = Email(name=second_account, ext=EMAIL.MAILTO_PLUS)
 
 
-def create_account(link="https://quizlet.com/", account_name=account, driver=driver1):
-    driver.get(link)
+def create_account(open_link="https://quizlet.com/", account_name=account, driver=driver1):
+    driver.get(open_link)
+
+    time.sleep(1)
+    driver.get_screenshot_as_file("screenshot.png")
 
     driver.find_element(By.CSS_SELECTOR, "[aria-label='Sign up']").click()
 
@@ -54,64 +65,69 @@ def create_account(link="https://quizlet.com/", account_name=account, driver=dri
     try:
         time.sleep(1)
         driver.find_element(By.NAME, "is_free_teacher").click()
-
     finally:
-        print("error when pressing teacher")
+        pass  # If it works , don't touch this.
 
     try:
         time.sleep(1)
         driver.find_element(By.NAME, "TOS").submit()
     finally:
-        print("error when pressing teacher")
-
-
-first_account_or_not = True
+        pass  # If it works , don't touch this.
 
 
 def check_email(data: EmailMessage, driver=driver1):
     urls = extractor.find_urls(data.text)
     driver.get(urls[1])
     data.delete()  # delete message
-    print(first_account_or_not)
-    if first_account_or_not:
-        refer_account()
 
 
-def refer_account(driver=driver1):
-    global first_account_or_not, links
-    first_account_or_not = False
-
+def refer_account(driver: webdriver = driver1) -> string:
     driver.get("https://quizlet.com/refer-a-teacher")
     driver.find_element(By.CSS_SELECTOR, "[aria-label='Copy link']").click()
 
-    win32clipboard.OpenClipboard()
-    links = win32clipboard.GetClipboardData()
-    win32clipboard.CloseClipboard()
+    copy_link: string = None
 
-    time.sleep(3)
+    for i in driver.find_elements(By.TAG_NAME, "input"):
+        try:
+            if i.get_attribute("value").startswith("https://quizlet.com/teacher-referral-invite"):
+                copy_link = i.get_attribute("value")
+                break
+        finally:
+            pass
+    return copy_link
 
 
 if __name__ == '__main__':
+    print(f"Create first account for {account}")
     create_account()
-    links = ""
 
-    while len(app1.get_all_message()) == 0:
+    print("waiting for emails......")
+    while len(email1.get_all_message()) == 0:
         continue  # wait until received message
-    check_email(app1.get_all_message()[0])
 
-    app1.destroy()
-    app1.close()
+    print("received email and opening......")
+    check_email(email1.get_all_message()[0])
+
+    link = refer_account()
+
+    print(f"the refer link is {link}")
+
+    email1.destroy()
+    email1.close()
     driver1.quit()
 
-    driver2 = webdriver.Chrome(service=s)
+    driver2 = webdriver.Chrome(service=s, options=option)
+    print(f"Create second account for {second_account}")
+    create_account(link, second_account, driver2)
 
-    create_account(links, secondEmail, driver2)
-
-    while len(app2.get_all_message()) == 0:
+    print("waiting for emails")
+    while len(email2.get_all_message()) == 0:
         continue  # wait until received message
-    check_email(app2.get_all_message()[0], driver2)
 
-    app2.destroy()
+    print("received email and opening......")
+    check_email(email2.get_all_message()[0], driver2)
+
+    email2.destroy()
     driver2.quit()
 
     print("===============================================================================")
